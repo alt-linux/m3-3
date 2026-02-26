a2c2ad08b2fed2be981ebe2259a97dd21b5a1e8b
alt-linux <amogyous@gmail.com>
2026-02-26 19:54:13 +0300

    Add files via upload

--- m3-hq-rtr-3-1.sh
+++ m3-hq-rtr-3-1.sh
@@ -1,7 +1,2 @@
-apt-get install tcpdump -y
-rm -rf /etc/strongswan/ipsec.conf
-cp /down/ipsec.conf1 /etc/strongswan/ipsec.conf
-echo "10.5.5.1 10.5.5.2 :PSK "P@ssw0rd"" >> /etc/strongswan/ipsec.secrets
-systemctl enable --now strongswan
-systemctl enable --now strongswan-starter
+echo 'apt-get install tcpdump -y && rm -rf /etc/strongswan/ipsec.conf && cp /down/ipsec.conf1 /etc/strongswan/ipsec.conf && echo "10.5.5.1 10.5.5.2 :PSK "P@ssw0rd"" >> /etc/strongswan/ipsec.secrets && systemctl enable --now strongswan && systemctl enable --now strongswan-starter'
 echo "PROVERKA=sudo tcpdump -i tun -n -p icmp"
